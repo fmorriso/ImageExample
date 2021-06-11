0e78110eb191ee0b8f4fcb22b3ae86b1997f5007
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-06-11 09:47:07 -0400

    reformat code

--- Driver.java
+++ Driver.java
@@ -4,17 +4,19 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
-public class Driver {
+public class Driver
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
 		// capture size of screen we're using
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
 		// Define the size of the JFrame as a rectangle that is a percentage of the
 		// available device screen area, rounded to a multiple of 100, because it
-    // might make it easier to do calculations that position other objects within
-    // the frame.
+		// might make it easier to do calculations that position other objects within
+		// the frame.
 		final double pct = 90; // a value that I recommend be between 50 and 100
 		final int frameHeight = (int) (screenSize.height * pct / 100) / 100 * 100;
 		final int frameWidth = (int) (screenSize.width * pct / 100) / 100 * 100;
@@ -29,15 +31,15 @@ public class Driver {
 		// frame.pack() below:
 		frame.setPreferredSize(frameSize);
 
-    // Create the JPanel that sits inside the JFrame.
-    // Notice how we have to tell it the size of the "parent" JFrame
-    // in order to size it correctly.
+		// Create the JPanel that sits inside the JFrame.
+		// Notice how we have to tell it the size of the "parent" JFrame
+		// in order to size it correctly.
 		PicturePanel pnl = new PicturePanel(frameSize);
 		frame.getContentPane().add(pnl, BorderLayout.CENTER);
 
 		// This is key to making the JFrame and JPanel contents look correct,
-    // at least initially.
-    frame.pack();
+		// at least initially.
+		frame.pack();
 
 		// put the JFrame in the middle of the physical screen
 		frame.setLocationRelativeTo(null);
